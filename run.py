2d918093af4ecd42f5d97c08270b6fe4af6a4aa7
7renan <lima.renan96@gmail.com>
2023-08-17 10:55:20 -0300

    feat: create entities sqlalchemy

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import text
 from infra.db.settings.connection import DBConnectionHandler
 
 
+# models
+from infra.db.entities.filme import Filmes
+
+
 with DBConnectionHandler() as db:
-    session = db.session
-    response = session.execute(text('SELECT * FROM roles'))
-    print(response.all())
+    try:
+        session = db.session
+        filmes = session.query(Filmes).all()
+        print(filmes)
+    except SQLAlchemyError as e:
+        print("deu ruim", e)
 
     
